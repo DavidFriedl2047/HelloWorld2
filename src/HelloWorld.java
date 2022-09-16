e180eb04a8abe670f994b5a2dd33c6423259d204
DavidFriedl2047 <s51729@edu.campus02.at>
2022-09-16 19:13:10 +0200

    Inital Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Welcome to KONF 2022");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
+
+
+
+    }
+}
